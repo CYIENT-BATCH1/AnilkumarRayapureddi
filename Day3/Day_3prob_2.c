26b1b7bc164f8d0f455c4c28188722603474b6af
Anilkumar218 <108973289+Anilkumar218@users.noreply.github.com>
2022-07-11 17:26:58 +0530

    Update and rename Day_3prob_2.c.c to Day_3prob_2.c

--- Day3/Day_3prob_2.c
+++ Day3/Day_3prob_2.c
@@ -1,4 +1,4 @@
-2.Write a program to calculate the sum of the first and the second last digit of a 5-digit number entered from the keyboard.
+//2.Write a program to calculate the sum of the first and the second last digit of a 5-digit number entered from the keyboard.
 
 
 #include <stdio.h>
