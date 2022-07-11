e9662f7f14a123a4b60fa99d138b160953626a66
Anilkumar218 <108973289+Anilkumar218@users.noreply.github.com>
2022-07-11 17:24:23 +0530

    Create Day_3prob_1.c

---
+++ Day3/Day_3prob_1.c
@@ -0,0 +1,12 @@
+1.Convert 0xFF to binary value
+#include <stdio.h>
+
+int main()
+{
+    int n=0xff,i,x;declare a varible and assign a given value
+    for(i=15;i>=0;i--){
+        x=(n>>i)&1;//using bitwise operator to convert the binary value
+    
+    printf("%d",x);
+}
+}
