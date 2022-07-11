370f91aa2552939962c723d2e358d7cb86a93216
Anilkumar218 <108973289+Anilkumar218@users.noreply.github.com>
2022-07-11 17:46:44 +0530

    Create Day_4prob_1.c

---
+++ Day4/Day_4prob_1.c
@@ -0,0 +1,28 @@
+//A)ARITHMATICAL OPERATOR 
+
+x=30, y=20, z=10 FIND OUT THE VALUE FOR K
+
+1)k = x + y * z / 4 % 2 - 1 ;
+ 
+ answer:29
+
+2)k= x% y+z*z;
+
+  answer:110
+
+3)k=  x*x-y/z;
+
+ answer:898
+
+4)k= x+y/z-y; 
+
+  answer:12
+
+5)y=x+y-(x=y); //y=?
+
+  answer:30
+
+6)y=x+y-x=y    //y=?
+ 
+  answer:error
+
